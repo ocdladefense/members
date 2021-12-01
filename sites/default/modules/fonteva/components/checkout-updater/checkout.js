81c1fded30e2722074cb0475d428349608727baf
trevoruehlinx1 <trevoruehlinx1@gmail.com>
2021-11-30 16:19:59 -0800

    remove all f modules.

--- sites/default/modules/fonteva/components/checkout-updater/checkout.js
+++
@@ -1,7 +0,0 @@
-$(function(){
-	$("h3.m-t-30").html("<strong>IABC Membership Renewal Checkout</strong>");
-	
-	$("span.bg-gray-lighter").css({display:"none"});
-	
-	$("#newUserOptionPassword").css({visibility:"hidden"});
-});
